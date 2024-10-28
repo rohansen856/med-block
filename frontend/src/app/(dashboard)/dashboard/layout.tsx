5fae558452ba467f3f617594d3c87036d6bcfb48
rohansen856 <rohansen856@gmail.com>
2024-10-28 10:14:40 +0530

    unneccesary assets removed from public dir + dashboard page fixes

--- frontend/src/app/(dashboard)/dashboard/layout.tsx
+++ frontend/src/app/(dashboard)/dashboard/layout.tsx
@@ -5,6 +5,7 @@ import { getCurrentUser } from "@/lib/session"
 import { MainNav } from "@/components/shared/main-nav"
 import { DashboardNav } from "@/components/shared/nav"
 import { SiteFooter } from "@/components/shared/site-footer"
+import { DashboardSidebar } from "@/components/sidebar"
 import { UserAccountNav } from "@/components/user-account-nav"
 
 interface DashboardLayoutProps {
@@ -21,28 +22,26 @@ export default async function DashboardLayout({
   }
 
   return (
-    <div className="flex min-h-screen flex-col space-y-6">
-      <header className="sticky top-0 z-40 border-b bg-background">
-        <div className="container flex h-16 items-center justify-between py-4">
-          <MainNav items={dashboardConfig.mainNav} />
-          <UserAccountNav
-            user={{
-              name: user.name,
-              image: user.image,
-              email: user.email,
-            }}
-          />
-        </div>
-      </header>
-      <div className="container grid flex-1 gap-12 md:grid-cols-[200px_1fr]">
-        <aside className="hidden w-[200px] flex-col md:flex">
-          <DashboardNav items={dashboardConfig.sidebarNav} />
-        </aside>
-        <main className="flex w-full flex-1 flex-col overflow-hidden">
-          {children}
-        </main>
+    <div className="flex min-h-screen">
+      <aside>
+        <DashboardSidebar user={user} />
+      </aside>
+      <div className="w-full">
+        <header className="sticky top-0 z-40 border-b bg-background py-2">
+          <div className="container flex h-16 items-center justify-between py-4">
+            <MainNav items={dashboardConfig.mainNav} />
+            <UserAccountNav
+              user={{
+                name: user.name,
+                image: user.image,
+                email: user.email,
+              }}
+            />
+          </div>
+        </header>
+
+        <main className="px-4">{children}</main>
       </div>
-      <SiteFooter className="border-t" />
     </div>
   )
 }
--- frontend/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/src/app/(dashboard)/dashboard/page.tsx
@@ -35,29 +35,5 @@ export default async function DashboardPage() {
     },
   })
 
-  return (
-    <DashboardShell>
-      <DashboardHeader heading="Posts" text="Create and manage posts.">
-        <PostCreateButton />
-      </DashboardHeader>
-      <div>
-        {posts?.length ? (
-          <div className="divide-y divide-border rounded-md border">
-            {posts.map((post) => (
-              <PostItem key={post.id} post={post} />
-            ))}
-          </div>
-        ) : (
-          <EmptyPlaceholder>
-            <EmptyPlaceholder.Icon name="post" />
-            <EmptyPlaceholder.Title>No posts created</EmptyPlaceholder.Title>
-            <EmptyPlaceholder.Description>
-              You don&apos;t have any posts yet. Start creating content.
-            </EmptyPlaceholder.Description>
-            <PostCreateButton variant="outline" />
-          </EmptyPlaceholder>
-        )}
-      </div>
-    </DashboardShell>
-  )
+  return <DashboardShell></DashboardShell>
 }
