844a36ca39b1f1d350af1f370ebf27a3b7491004
rohansen856 <rohansen856@gmail.com>
2024-11-08 23:04:09 +0530

    final push

--- ai-ml/rag-tutorial-v2/get_embedding_function.py
+++
@@ -1,7 +0,0 @@
-from langchain_community.embeddings.ollama import OllamaEmbeddings
-from langchain_community.embeddings.bedrock import BedrockEmbeddings
-
-
-def get_embedding_function():
-    embeddings = OllamaEmbeddings(model="llama3.1")
-    return embeddings
--- blockchain/nextjs-frontend/src/lib/stripe.ts
+++
@@ -1,7 +0,0 @@
-import Stripe from "stripe"
-
-import { env } from "@/env.mjs"
-
-export const stripe = new Stripe(env.STRIPE_API_KEY, {
-  typescript: true,
-})
